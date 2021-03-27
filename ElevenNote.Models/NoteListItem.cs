fa80c0ba5c414d49ac367434bc9a5072419478e4
dawita <dawit.fitru@gmail.com>
2021-03-27 17:32:07 -0400

    Not controller , note ListItem and Index View are add on this commit

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+ //for handling task of collecting property data we wamt the user to eneter specific data and we want to show specific data in specific view
+    public class NoteListItem
+    {
+        public int NoteId  { get; set; }
+        public string Title  { get; set; }
+        [Display(Name="Created")]// this show up later
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
